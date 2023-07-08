368c02457e6fbab6461ea33e515650861780338f
unknown <danilaplatonov209@gmail.com>
2023-07-08 11:15:04 +0300

    secondCommit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const cors = require('cors');
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const authRouter = require('./authRoutes');
@@ -16,15 +16,20 @@ const BD_URL = `mongodb+srv://danila:355473288@cluster0.ag5ij.mongodb.net/?retry
 const app = express();
 mongoose.set('strictQuery', false);
 
-const cors = require('cors');
-const corsOptions ={
-    origin:true, 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
-    methods: "GET,HEAD,PUT,POST,DELETE"
-}
-
-app.use(cors(corsOptions));
+app.use(cors());
+app.use(function (req, res, next) {
+// Website you wish to allow to connect
+res.setHeader('Access-Control-Allow-Origin', '*');
+// Request methods you wish to allow
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// Request headers you wish to allow
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+// Set to true if you need the website to include cookies in the requests sent
+// to the API (e.g. in case you use sessions)
+res.setHeader('Access-Control-Allow-Credentials', true);
+// Pass to next layer of middleware
+next();
+});
 app.use(fileUpload())
 app.use(cors());
 app.use(express.json());

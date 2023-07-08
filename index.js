923f734035af8e400f3b65bab96ff4f0ee0a5a30
unknown <danilaplatonov209@gmail.com>
2023-07-08 11:41:30 +0300

    secondCommit

--- index.js
+++ index.js
@@ -16,20 +16,12 @@ const BD_URL = `mongodb+srv://danila:355473288@cluster0.ag5ij.mongodb.net/?retry
 const app = express();
 mongoose.set('strictQuery', false);
 
-app.use(cors());
-app.use(function (req, res, next) {
-// Website you wish to allow to connect
-res.setHeader('Access-Control-Allow-Origin', '*');
-// Request methods you wish to allow
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-// Request headers you wish to allow
-res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-// Set to true if you need the website to include cookies in the requests sent
-// to the API (e.g. in case you use sessions)
-res.setHeader('Access-Control-Allow-Credentials', true);
-// Pass to next layer of middleware
-next();
-});
+var corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));s
+
 app.use(fileUpload())
 app.use(cors());
 app.use(express.json());

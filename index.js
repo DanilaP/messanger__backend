0e6ed542a81562d4555b95ffaddee6d4db23cf3c
unknown <danilaplatonov209@gmail.com>
2023-07-08 11:50:31 +0300

    secondCommit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-app.use(cors(corsOptions));s
+app.use(cors(corsOptions));
 
 app.use(fileUpload())
 //app.use(cors());
